c98b7ecd06c13ec84ba33d7ad86555796141f43b
abab <ababup1192@gmail.com>
2019-03-06 01:28:56 +0900

    定期実行リファクタ

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -31,35 +31,34 @@ modeTest testCase currentMode msg expectedMode =
                 |> Expect.equal expectedMode
 
 
+tickTest : String -> Mode -> Int -> Test
+tickTest testCase mode expectedCount =
+    test testCase <|
+        \() ->
+            update (Tick <| Time.millisToPosix 0) (Model 0 mode)
+                |> Tuple.first
+                |> .count
+                |> Expect.equal expectedCount
+
+
 updateTest : Test
 updateTest =
     describe "updateのテスト" <|
-        [ countTest "カウンタが0のとき、IncrementN 3 されると3になる" 0 (IncrementN 3) 3
-        , countTest "カウンタが5のとき、IncrementN 5 されると10になる" 5 (IncrementN 5) 10
-        , countTest "カウンタが5のとき、DecrementN 5 されると0になる" 5 (DecrementN 5) 0
-        , countTest "カウンタが1のとき、DecrementN -3 されると-2になる" 1 (DecrementN 3) -2
-        , modeTest "+ボタンがクリックされたとき、IncrementModeになる" StopMode Increment IncrementMode
-        , modeTest "-ボタンがクリックされたとき、DecrementModeになる" StopMode Decrement DecrementMode
-        , modeTest "カウントがクリックされたとき、ストップ" IncrementMode Stop StopMode
-        , describe "カウント増減時間が来たかつ、カウントが0のとき"
-            [ test "IncrementModeのとき 1になる" <|
-                \() ->
-                    update (Tick <| Time.millisToPosix 0) (Model 0 IncrementMode)
-                        |> Tuple.first
-                        |> .count
-                        |> Expect.equal 1
-            , test "DecrementModeのとき 1になる" <|
-                \() ->
-                    update (Tick <| Time.millisToPosix 0) (Model 0 DecrementMode)
-                        |> Tuple.first
-                        |> .count
-                        |> Expect.equal -1
-            , test "StopModeのとき 0のまま" <|
-                \() ->
-                    update (Tick <| Time.millisToPosix 0) (Model 0 StopMode)
-                        |> Tuple.first
-                        |> .count
-                        |> Expect.equal 0
+        [ describe "n増減"
+            [ countTest "カウンタが0のとき、IncrementN 3 されると3になる" 0 (IncrementN 3) 3
+            , countTest "カウンタが5のとき、IncrementN 5 されると10になる" 5 (IncrementN 5) 10
+            , countTest "カウンタが5のとき、DecrementN 5 されると0になる" 5 (DecrementN 5) 0
+            , countTest "カウンタが1のとき、DecrementN -3 されると-2になる" 1 (DecrementN 3) -2
+            ]
+        , describe "モード切替"
+            [ modeTest "+ボタンがクリックされたとき、IncrementModeになる" StopMode Increment IncrementMode
+            , modeTest "-ボタンがクリックされたとき、DecrementModeになる" StopMode Decrement DecrementMode
+            , modeTest "カウントがクリックされたとき、ストップ" IncrementMode Stop StopMode
+            ]
+        , describe "定期実行。カウント増減時間が来たかつ、カウントが0のとき"
+            [ tickTest "IncrementModeのとき 1になる" IncrementMode 1
+            , tickTest "DecrementModeのとき -1になる" DecrementMode -1
+            , tickTest "StopModeのとき 0になる" StopMode 0
             ]
         ]
 
